5dfdbea5f11560f936443c867d6bf3da9f690807
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-10-10 15:28:47 -0700

    basic navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+import Image from "next/image";
+
+function NavItem({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  let isActive = usePathname() === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={clsx(
+          "relative block px-3 py-2 transition",
+          isActive
+            ? "text-teal-500 dark:text-teal-400"
+            : "hover:text-teal-500 dark:hover:text-teal-400"
+        )}
+      >
+        {children}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+        )}
+      </Link>
+    </li>
+  );
+}
+
+function DesktopNavigation(props: React.ComponentPropsWithoutRef<"nav">) {
+  const routes = [
+    { href: "/about", label: "About" },
+    { href: "/about", label: "Projects" },
+    { href: "/about", label: "Work" },
+  ];
+  return (
+    <nav {...props}>
+      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        {routes.map((route, index) => (
+          <NavItem key={index} href={route.href}>
+            {route.label}
+          </NavItem>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+function ThemeToggle() {
+  let { resolvedTheme, setTheme } = useTheme();
+  let otherTheme = resolvedTheme === "dark" ? "light" : "dark";
+  let [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <button
+      type="button"
+      aria-label={mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"}
+      className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20 h-12"
+      onClick={() => setTheme(otherTheme)}
+    >
+      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
+      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
+    </button>
+  );
+}
+
+function Avatar() {
+  // 5:4 ratio
+  return (
+    <Image
+      width={50}
+      height={40}
+      src="/profile.png"
+      alt="morris profile pic :3"
+      className="rounded-b-full object-cover"
+    />
+  );
+}
+
+export function Header() {
+  return (
+    <header className="p-4">
+      <div className="flex mx-auto max-w-2xl md:max-w-5xl justify-between">
+        <Avatar />
+        <DesktopNavigation />
+        <ThemeToggle />
+      </div>
+    </header>
+  );
+}
