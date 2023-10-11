6052cd2a32a5229ffe83032c2770dba13709bfd9
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-10-10 20:46:30 -0700

    uploaded morris pics to sanity assets

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,28 +1,5 @@
 import { type SchemaTypeDefinition } from "sanity";
 
-const morris_pic = {
-  name: "morris-pic",
-  type: "document",
-  title: "Morris Picture",
-  fields: [
-    {
-      name: "alt",
-      type: "string",
-      title: "Alt Text",
-    },
-    {
-      name: "src",
-      type: "string",
-      title: "Image Source",
-    },
-    {
-      name: "id",
-      type: "number",
-      title: "Picture ID",
-    },
-  ],
-};
-
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [morris_pic],
+  types: [],
 };
