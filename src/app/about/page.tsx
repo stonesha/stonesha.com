d91e298bbe106c587db483b026ff4c423eb8ce1e
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-10-11 21:38:31 -0700

    changed routes

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About",
-};
-
-export default function About() {
-  return <div>In-Progress</div>;
-}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,15 @@ import DesktopNavigation from "~/components/header/DesktopNavigation";
 import ThemeToggle from "~/components/header/ThemeToggle";
 
 export default function Header() {
+  const routes = [
+    { href: "/projects", label: "Projects" },
+    { href: "/work", label: "Work" },
+  ];
   return (
     <header className="p-4">
       <div className="flex mx-auto max-w-2xl md:max-w-5xl justify-between">
         <Avatar />
-        <DesktopNavigation />
+        <DesktopNavigation routes={routes} />
         <ThemeToggle />
       </div>
     </header>
