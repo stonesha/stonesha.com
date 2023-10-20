9b016c7da99bec1bd37f21625921d44605758d74
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-10-19 20:52:17 -0700

    flipping cdn switch

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -5,5 +5,5 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-11", // https://www.sanity.io/docs/api-versioning
   // disabling cache for now, will probably turn on after finishing website
-  useCdn: false, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+  useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 });
