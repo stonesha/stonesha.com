0c7c8a3efcde0fb507bc1246545f9cf17c599b86
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-10-19 21:13:35 -0700

    Update page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -16,7 +16,7 @@ export default async function Work() {
     <>
       <div className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Places I&apos;ve worked at
+          Work
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           These are the places I&apos;ve worked at professionally; whether it
