bd690ad62ba7f3251e8f67d8c60dbdacf7d83cc9
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-11-05 21:13:32 -0800

    added light mode dark mode support

--- src/components/CustomCodeBlock.tsx
+++ src/components/CustomCodeBlock.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { CopyBlock, dracula } from "react-code-blocks";
+import { CodeBlock, atomOneDark, atomOneLight } from "react-code-blocks";
 import type { Snippet } from "~/utils/sanity/schema/snippet";
 import { Disclosure, Transition } from "@headlessui/react";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { useTheme } from "next-themes";
 
 export function CustomCodeBlock(snippet: Snippet) {
+  let { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === "light" ? atomOneLight : atomOneDark;
+
   return (
     <Disclosure as="div">
       {({ open }) => (
         <>
-          <Disclosure.Button className="flex w-full justify-between rounded-lg bg-zinc-100 px-4 py-2 text-left text-lg font-medium text-zinc-900 hover:bg-zinc-200 focus:outline-none focus-visible:ring focus-visible:ring-zinc-500/75 mb-1 items-center">
+          <Disclosure.Button
+            className="flex w-full justify-between rounded-lg 
+          bg-zinc-100 px-4 py-2 text-left text-lg font-medium text-zinc-900 dark:hover:bg-zinc-700/50
+           hover:bg-zinc-200 mb-1 items-center dark:text-zinc-100 dark:bg-zinc-800/50"
+          >
             {snippet.title}
             <ChevronRightIcon
               className={`${open ? "rotate-90 transform" : ""} w-8 h-8`}
@@ -25,15 +33,12 @@ export function CustomCodeBlock(snippet: Snippet) {
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-95 opacity-0"
           >
-            <Disclosure.Panel>
-              <CopyBlock
-                {...{
-                  language: snippet.code.language,
-                  showLineNumbers: true,
-                  text: snippet.code.code,
-                  theme: dracula,
-                  codeBlock: true,
-                }}
+            <Disclosure.Panel as="div" className="shadow-md rounded-md">
+              <CodeBlock
+                language={snippet.code.language}
+                showLineNumbers
+                text={snippet.code.code}
+                theme={theme}
               />
             </Disclosure.Panel>
           </Transition>
