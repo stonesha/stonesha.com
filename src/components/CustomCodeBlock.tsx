46d5f798d29245b897e8296209cfd08e2538c80e
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-11-05 21:29:44 -0800

    added copy button
    copyblock has some weird errors on latest ver of react/next

--- src/components/CustomCodeBlock.tsx
+++ src/components/CustomCodeBlock.tsx
@@ -5,8 +5,14 @@ import type { Snippet } from "~/utils/sanity/schema/snippet";
 import { Disclosure, Transition } from "@headlessui/react";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import { useTheme } from "next-themes";
+import {
+  ClipboardDocumentListIcon,
+  ClipboardDocumentCheckIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export function CustomCodeBlock(snippet: Snippet) {
+  let [copied, setCopied] = useState(false);
   let { resolvedTheme } = useTheme();
   const theme = resolvedTheme === "light" ? atomOneLight : atomOneDark;
 
@@ -40,6 +46,19 @@ export function CustomCodeBlock(snippet: Snippet) {
                 text={snippet.code.code}
                 theme={theme}
               />
+              <button
+                onClick={() => {
+                  navigator.clipboard.writeText(snippet.code.code);
+                  setCopied((copied) => !copied);
+                }}
+                className="absolute top-5 right-5 p-2 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-200/20"
+              >
+                {copied ? (
+                  <ClipboardDocumentCheckIcon className="w-5 h-5 text-teal-400" />
+                ) : (
+                  <ClipboardDocumentListIcon className="w-5 h-5 text-zinc-900 dark:text-zinc-100" />
+                )}
+              </button>
             </Disclosure.Panel>
           </Transition>
         </>
